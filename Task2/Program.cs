31f6085689c6bddaef9671e007e8584f4c816005
IvanKovalev <deformgame@gmail.com>
2022-08-26 12:53:54 +0300

    Added Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string number = Convert.ToString(num);
+
+if (number.Length > 2)
+{
+    Console.WriteLine($"Третья цифра в числе {num} - {number[2]}. ");
+}
+else
+{ 
+    Console.WriteLine($"В числе {num} меньше трех цифр. ");
+}
